bfbd13b04acdb231ce26d6bf8b04f47d76d81dd2
Jake Dusing <dusingjake@gmail.com>
2024-08-21 16:10:17 -0500

    start search input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchInput from "./SearchInput";
 
 export default function Header() {
   return (
     <div>
       <Link to="/">Legend of Zelda</Link>
+      <SearchInput />
     </div>
   );
 }
