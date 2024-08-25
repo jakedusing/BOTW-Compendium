b3143406fceb8628af2e796ce00169ba6c3c2cb3
Jake Dusing <dusingjake@gmail.com>
2024-08-25 18:47:23 -0500

    add monsterdetails interface

---
+++ src/api/types/categoryMonsterDetails.ts
@@ -0,0 +1,8 @@
+export interface CategoryMonsterDetails {
+  name: string;
+  category: string;
+  description: string;
+  common_locations: string[];
+  drops: string[];
+  image: string;
+}
--- src/components/CategoryListItem.tsx
+++ src/components/CategoryListItem.tsx
@@ -20,25 +20,27 @@ export default function CategoryListItem({ category }: CategoryListItemProps) {
   );
 
   return (
-    <div className="border p-4 rounded-lg bg-white shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out flex flex-col justify-between">
+    <div className="border p-4 rounded-lg bg-green-100 shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out flex flex-col justify-between">
       <div className="flex flex-col gap-2 mb-4">
         <Link
           to={`/categories/${category.name}`}
-          className="text-xl font-semibold text-blue-600 hover:underline"
+          className="text-xl font-semibold text-green-800 hover:underline"
         >
           {category.name?.toUpperCase()}
         </Link>
-        <p className="text-sm text-gray-600">{category.description}</p>
+        <p className="text-sm text-gray-800">{category.description}</p>
         <div className="flex flex-wrap gap-2 mt-2">{renderedLocations}</div>
         <img
           src={`https://botw-compendium.herokuapp.com/api/v3/compendium/entry/${category.name}/image`}
+          alt={category.name}
+          className="w-full h-auto object-cover rounded"
         />
       </div>
 
       <div className="mr-6 ">
         <Link
           to={`categories/${category.name}`}
-          className="block py-2 px-4 rounded bg-black text-white text-center text-lg hover:bg-gray-800 transition-colors duration-300 ease-in-out"
+          className="block py-2 px-4 rounded bg-yellow-600 text-white text-center text-lg hover:bg-yellow-500 transition-colors duration-300 ease-in-out"
         >
           View
         </Link>
